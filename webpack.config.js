9e72394e077b368b11c54fa298729d9ddf551414
CamiloRestrepo5 <camilorpo10@gmail.com>
2023-04-10 21:29:53 -0500

    configuracion del webpack

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+// sirve para identificar la ruta de donde se encuentra este archivo
+const path = require('path');
+
+// me permite trabajar con documentos Html
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// extraer el codigo Css, minificarlo y optimizarlo. Ademas lo agrega como parte del head
+const MiniCsssExtractPlugin = require('mini-css-extract-plugin')
+
+// nos permite copiar archivos de una ruta a otra 
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+// creamos una funcion de flecha
+module.exports = (env, argv) => {
+
+    const isProduction = argv.mode === 'production';
+
+    // para el servidor:
+    return {
+        entry: {
+            index: './src/index.js',
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        module: {
+            rules: [
+
+                {
+                    test: /\.css$/,
+                    use: [
+                        isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+                        'css-loader'
+
+                    ]
+                },
+                {
+                    test: /\.js$/,
+                    include: path.resolve(__dirname, 'src/assets/js'),
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                }
+            ]
+        },
+        plugins: [],
+        devServer: {
+            static: {
+                directory: path.join(__dirname, 'dist'),
+            },
+            open: true,
+            hot: true,
+            watchFiles: [
+                'src/**/*'
+            ]
+        }
+    };
+
+}
