e04c98305e68a9f9cf230490cabde0359fdcc2d3
CamiloRestrepo5 <camilorpo10@gmail.com>
2023-04-27 13:35:34 -0500

    validación de correo electrónico usando expresiones regulares

--- src/assets/js/teachers/repository.js
+++ src/assets/js/teachers/repository.js
@@ -22,3 +22,8 @@ export function createTeacher(teacher) {
 export function readTeachers() {
     return getDatabase(dbName);
 }
+
+export function findTeacherById(idTeacher) {
+    
+    return readTeachers().find(({id}) => id === parseInt(idTeacher));
+}
